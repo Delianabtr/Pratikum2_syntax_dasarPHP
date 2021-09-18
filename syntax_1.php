a8605a440957bb8c23412b5551f432d4ad685302
deliana <delianabtr30@gmail.com>
2021-09-19 00:26:22 +0700

    membuat syntax dengan 2 variable

---
+++ syntax_1.php
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        <?php
+            $jalan = "Medan - B.Aceh";
+            $noRumah = 29;
+            $blok = "4C";
+            $jumlahPenghuni = 3;
+            echo "Hasil Variabel Variabel jalan +
+            variabel noRumah adalah";
+            echo $alamat = $jalan . $noRumah;
+            echo "<br>";
+            echo "Hasil Variabel variabel noRumah
+            + variabel jumlahPenghuni adalah ";
+            echo $hasil = $blok + $noRumah;
+            echo "<br>";
+            ?>
+    </body>
+</html>
