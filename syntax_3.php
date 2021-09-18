c035aafabe4d9246c16bcff4500551dea4eb81c7
deliana <delianabtr30@gmail.com>
2021-09-19 02:45:53 +0700

    Membuat operasi aritmatika 1 pada php

---
+++ syntax_3.php
@@ -0,0 +1,22 @@
+<html>
+    <body>
+        <?
+        $a=10;
+        $b=3;
+        echo "\$a = $a <BR>";
+        echo "\$b = $b <BR>";
+        echo '$a + $b = ';
+        print $a+$b;
+        echo "<BR>";
+        echo '$a - $b = ';
+        echo "<BR>"
+        echo '$a * $b = ';
+        print $a*$b;
+        echo "<BR>";
+        echo '$a / $b = ';
+        print $a/$b;
+        echo '$a % $b = ';
+        print '$a%$b = ';
+        ?>
+    </body>
+</html>
