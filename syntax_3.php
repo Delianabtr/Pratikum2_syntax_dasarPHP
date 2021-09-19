fc806f86cfcd6fe032ad90ff92a441f17a9948c3
deliana <delianabtr30@gmail.com>
2021-09-19 15:59:20 +0700

    mengubah syntaxyang eror

--- syntax_3.php
+++ syntax_3.php
@@ -1,24 +1,25 @@
 <html>
-    <body>
-        <?
-        $a=10;
-        $b=3;
-        echo "\$a = $a <BR>";
-        echo "\$b = $b <BR>";
-        echo '$a + $b = ';
-        print $a+$b;
-        echo "<BR>";
-        echo '$a - $b = ';
-        print $a-$b;
-        echo "<BR>"
-        echo '$a * $b = ';
-        print $a*$b;
-        echo "<BR>";
-        echo '$a / $b = ';
-        print $a/$b;
-        echo "<BR>";
-        echo '$a % $b = ';
-        print '$a%$b = ';
-        ?>
-    </body>
+<body>
+    <?php
+    $a = 10;
+    $b = 3;
+    echo "\$a = $a <br>";
+    echo "\$b = $b <br>";
+    echo '$a +$b = ';
+    print $a + $b;
+    echo "<br>";
+    echo '$a - $b = ';
+    print $a - $b;
+    echo "<br>";
+    echo '$a * $b = ';
+    print $a * $b;
+    echo "<br>";
+    echo '$a / $b = ';
+    print $a / $b;
+    echo "<br>";
+    echo '$a & $b = ';
+    print $a & $b;
+    echo "<br>";
+    ?>
+</body>
 </html>
